603e52c0ba0cecef1befd1d1fe68af327da64b7a
JuanjoRestrepo <restrepojuanjo@gmail.com>
2025-03-05 11:22:44 -0500

    First breakpoints added on phone look

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
+/*============= Active Link ============= */
+const navlink = document.querySelectorAll('.nav__link');
+
+function activeLink(){
+    navlink.forEach((a) => a.classList.remove('active-link'));
+    this.classList.add('active-link');
+}
+
+navlink.forEach((a) => a.addEventListener('click', activeLink));
+
+
+/*============= Background Header ============= */
+function scrollHeader(){
+    const header = document.getElementById('header');
+    // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY  >= 50){
+        header.classList.add('scroll-header');
+    }else{
+        header.classList.remove('scroll-header');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
 /*============= Mixitup Filter ============= */
 let mixerProjects = mixitup('.projects__container', {
    selectors: {
